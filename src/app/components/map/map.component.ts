2a6bc7babd294c418fb2fa0fe4ba97ec49b10c78
Wilmer Blanco <wilmervzla2000@gmail.com>
2024-11-10 00:19:45 -0300

    Add OpenStreetMap tiles to map component
    Integrated OpenStreetMap tile layer into the map component for enhanced map rendering. Updated angular.json to include Leaflet CSS for proper styling of map elements.

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -12,14 +12,23 @@ import { Map } from 'leaflet';
 export class MapComponent implements AfterViewInit {
   private map: Map;
 
+  private initMap(): void {
+    this.map = leaflet.map('map', {
+      center: [ 39.8282, -98.5795 ],
+      zoom: 3
+    });
+
+    const tiles = leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 18,
+      minZoom: 3,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
+
+    tiles.addTo(this.map);
+  }
+
   ngAfterViewInit(): void {
     this.initMap();
   }
 
-  private initMap(): void {
-    this.map = leaflet.map('map', {
-      center: [39.8282, -98.5795],
-      zoom: 3,
-    });
-  }
 }
